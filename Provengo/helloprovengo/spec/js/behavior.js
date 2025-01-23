f7793a4e9fad4ef4f46785ac6a6d839cb58100df
guyko29 <168747799+guyko29@users.noreply.github.com>
2025-01-23 18:15:01 +0200

    Update behavior.js

--- Provengo/helloprovengo/spec/js/behavior.js
+++ Provengo/helloprovengo/spec/js/behavior.js
@@ -1,32 +1,25 @@
 /* @provengo summon selenium */
 
-bthread('Admin login', function() {
-  let s = new SeleniumSession('setup admin').start(prestaShopAdminLoginURL)
-  s.adminLogin()
+
+// bthread('Admin login', function() {
+//   let s = new SeleniumSession('setup admin').start(prestaShopAdminLoginURL)
+//   s.adminLogin()
+// })
+
+bthread('userAddProductToWishlist', function () {
+  let userSession = new SeleniumSession("userSession").start("https://localhost/prestashop");
+  sync({request:Event("End(loginUser)")}, loginUser(userSession, {email: 'yali4343@gmail.com', password: '0548005118'}));
+  sync({request:Event("End(searchForProduct)")}, searchForProduct(userSession, {productName: 'Hummingbird printed t-shirt'}));
+  sync({request:Event("End(addProductToWishlist)")}, addProductToWishlist(userSession));
+  sync({request:Event("End(checkProductAdded)")}, checkProductAdded(userSession));
 })
 
-bthread('Add Product to Wishlist', function () {
-  let session = new SeleniumSession('add-product-session').start(prestaShopURL);
 
-  session.addProductToWishlist({
-    productName: 'Hummingbird printed t-shirt',
-  });
-
-  session.close(); // Close the browser session
-});
-
-bthread('Check Product Added', function () {
-  let session = new SeleniumSession('check-product-session').start(prestaShopURL);
-
-  session.checkProductAdded({
-    expectedWishlistCount: 1, // מספר המוצרים הצפוי ב-Wishlist לאחר ההוספה
-  });
-
-  session.close(); // Close the browser session
-});
-
-
-
+bthread("MakeSureStudentDontMarkADeletedCourse2", function() {
+  sync({waitFor:Event("End(checkProductAdded)")});
+  sync({waitFor:Event("End(addProductToWishlist)")});
+  sync({waitFor:Event("End(searchForProduct)")});
+})
 
 
 
